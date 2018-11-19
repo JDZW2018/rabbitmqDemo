ed9f70d8ca2cbc5fd23134bf288df6c86fad393c
jdzw2018 <949465194@qq.com>
2018-11-19 14:37:08 +0800

    init

--- rabbitmqAPI/src/main/java/cn/com/yusys/Application.java
+++ rabbitmqAPI/src/main/java/cn/com/yusys/Application.java
@@ -1,8 +1,20 @@
 package cn.com.yusys;
 
+import cn.com.yusys.cosumer.CosumerTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 /**
  * @author Tianfusheng
  * @date 2018/11/18
  */
+@SpringBootApplication
 public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
 }
---
+++ rabbitmqAPI/src/main/java/cn/com/yusys/send/SendTest.java
@@ -0,0 +1,22 @@
+package cn.com.yusys.send;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SendTest {
+    @Autowired
+    private AmqpTemplate template;
+
+    @ResponseBody
+    @RequestMapping(value = "/send", method = RequestMethod.GET)
+    public String demo(String msg) {
+        template.convertAndSend("testExchange", "testRoutingKey", msg);
+        return "success";
+    }
+}
