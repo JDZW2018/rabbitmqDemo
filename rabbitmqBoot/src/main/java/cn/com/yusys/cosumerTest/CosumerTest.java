cee4045996e058e86290541104432ca22439e631
wdym <949465194@qq.com>
2018-11-22 23:09:32 +0800

    123

---
+++ rabbitmqBoot/src/main/java/cn/com/yusys/cosumerTest/CosumerTest.java
@@ -0,0 +1,17 @@
+package cn.com.yusys.cosumerTest;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CosumerTest {
+
+    @Autowired
+    private CosumerTest cosumerTest;
+
+    @RabbitListener(queues = "testQueues")
+    public void demo(String msg){
+        System.out.println(msg);
+    }
+}
--- rabbitmqBoot/src/main/java/cn/com/yusys/sendTest/SendTest.java
+++ rabbitmqBoot/src/main/java/cn/com/yusys/sendTest/SendTest.java
@@ -5,18 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @author Tianfusheng
- * @date 2018/11/18
- */
 @Controller
 public class SendTest {
     @Autowired
-    private AmqpTemplate amqpTemplate;
+    private AmqpTemplate template;
 
+    @ResponseBody
     @RequestMapping(value = "/send", method = RequestMethod.GET)
-    public void send(String msg) {
+    public String demo(String msg) {
+        template.convertAndSend("testExchange", "testRoutingKey", msg);
+        return "success";
     }
-
 }
